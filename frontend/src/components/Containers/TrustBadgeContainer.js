864c4308729d1674a05a2ec659ce8dcd56eea9d0
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-10 21:57:43 +0800

    Revert "add grid & items with card & media card"
    This reverts commit 673aea279cb9627f8caab0a3bd03e66c5b2e0992.

--- frontend/src/components/Containers/TrustBadgeContainer.js
+++ frontend/src/components/Containers/TrustBadgeContainer.js
@@ -1,84 +1,24 @@
-import {Grid, Typography , Card, CardMedia, Container} from "@material-ui/core";
+import {Avatar, Container, Typography} from "@material-ui/core";
 
 const TrustBadgeContainer = ({classes}) => {
     return (
         <Container disableGutters={true} maxWidth="xl">
-            <Grid
-                container
-                direction="row"
-                justify="space-evenly"
-                alignItems="center"
-                style={{ backgroundColor: "#FFF0E5", height: "80vh" }}
-                >
-                <Grid item xs={12}>
-                    <Typography
-                        variant="h2"
-                        align="center"
-                        >
-                            Trusted By 
-                        </Typography>
-                </Grid>
-                <Grid  item xs={3}>
-                    <Card >
-                        <CardMedia
-                            style={{height: 102, width: 102, borderRadius: "50%"}}
-                            image="https://avatars.githubusercontent.com/u/73719327?v=4"
-                            title="Craig Norford"
-                            />
-                        <Typography
-                            variant="h6"
-                            align="center"
-                            >
-                        Craig Norford
-                        </Typography>
-                    </Card>
-                </Grid>
-                <Grid item xs={3}>
-                    <Card>
-                        <CardMedia
-                            style={{height: 102, width: 102, borderRadius: "50%"}}
-                            title="Eugene Gohh"
-                            image="https://avatars.githubusercontent.com/u/64187129?v=4"
-                            />
-                        <Typography
-                            variant="h6"
-                            align="center"
-                            >
-                        Eugene Gohh
-                        </Typography>
-                    </Card>
-                </Grid>
-                <Grid item xs={3}>
-                    <Card>
-                        <CardMedia
-                            style={{height: 102, width: 102, borderRadius: "50%"}}
-                            title="Torey Littlefield"
-                            image="https://avatars.githubusercontent.com/u/52614742?v=4"
-                        />
-                       <Typography
-                            variant="h6"
-                            align="center"
-                            >
-                        Torey Littlefield
-                        </Typography>
-                    </Card>
-                </Grid>
-                <Grid item xs={3}>
-                    <Card>
-                        <CardMedia
-                            style={{height: 102, width: 102, borderRadius: "50%"}}
-                            title="Lorenzo Zarantonello"
-                            image="https://avatars.githubusercontent.com/u/18583152?v=4"
-                        />
-                       <Typography
-                            variant="h6"
-                            align="center"
-                            >
-                        Lorenzo Zarantonello
-                        </Typography>
-                    </Card>
-                </Grid>
-          </Grid>
+          <Typography
+            component="div"
+            style={{ backgroundColor: "#FFF0E5", height: "80vh" }}
+            variant="h3"
+            >
+            Trusted By
+            <Avatar alt="Remy Sharp" src="/broken-image.jpg">
+              B
+            </Avatar>
+            <Avatar
+              alt="Remy Sharp"
+              src="/broken-image.jpg"
+              className={classes.orange}
+              />
+            <Avatar src="/broken-image.jpg" />
+          </Typography>
         </Container>
     )
 };
