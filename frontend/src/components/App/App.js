e6b5004144759b1cf903d7d3bedec0da00848f25
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-11 09:49:00 +0800

    added primary color, fontfamily, h2 font weight

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -5,8 +5,17 @@ import Footer from "../Footer/Footer";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 
 const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#2F1160"
+    }
+  },
   typography: {
+    fontFamily: ['"Poppins"'],
     fontSize: 14,
+    h2: {
+      fontWeight: 750
+    }
   }
 });
 
