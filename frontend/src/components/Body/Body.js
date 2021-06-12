5ead8cb8d5e61818c81b46c3e0aab766ba72856c
Eugene <gzkgt11@gmail.com>
2021-06-12 14:10:42 +0800

    add text & image for first container

--- frontend/src/components/Body/Body.js
+++ frontend/src/components/Body/Body.js
@@ -5,36 +5,102 @@ import {
   Typography,
   Container,
   TextField,
+  Grid,
+  Box,
+  createMuiTheme,
+  responsiveFontSizes,
+  MuiThemeProvider,
 } from "@material-ui/core";
 import { useStyles } from "./BodyStyles";
 import { TrustBadgeContainer } from "../Containers/TrustBadgeContainer";
+import MacBook from "../../assets/images/MacBook.svg";
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
 
 export default function Body() {
   const classes = useStyles();
 
   return (
-    <React.Fragment>
-      <CssBaseline />
+    <MuiThemeProvider theme={theme}>
+      <React.Fragment>
+        <CssBaseline />
 
-      {/* First container */}
-      <Container
-        disableGutters={true}
-        maxWidth="xl"
-        style={{ backgroundColor: "#FFF0E5", height: "100vh" }}
-      >
-        <Typography component="div" variant="h1" className={classes.root}>
-          We innovate your <br /> online shopping <br /> experience
-          <br />
-        </Typography>
-      </Container>
+        {/* First container */}
+        <Container
+          disableGutters={true}
+          maxWidth={false}
+          style={{ backgroundColor: "#FFF0E5", height: "65ch" }}
+        >
+          <Grid container spacing={0} justify="center">
+            <Grid item style={{ flexGrow: 1 }} component="div" display="inline">
+              <Grid
+                container
+                spacing={0}
+                justify="center"
+                alignItems="stretch"
+                direction="column"
+              >
+                <Box
+                  m={1}
+                  textAlign="center"
+                  position="absolute"
+                  top={150}
+                  left="10%"
+                  lineHeight={2}
+                >
+                  <Typography
+                    variant="h2"
+                    className={classes.root}
+                    direction="column"
+                    container
+                  >
+                    We innovate your <br /> online shopping <br /> experience
+                    <br />
+                  </Typography>
+                  <br />
+                  <Typography
+                    variant="subtitle1"
+                    gutterBottom
+                    className={classes.subtitle1}
+                    container
+                  >
+                    What do you wanna shop today? <br /> What do you need? Just
+                    tell us.
+                  </Typography>
+                </Box>
+              </Grid>
+            </Grid>
 
-      {/* Second Container */}
+            <Grid item style={{ flexGrow: 1 }} component="div" display="inline">
+              <Grid
+                container
+                spacing={0}
+                justify="center"
+                alignItems="stretch"
+                direction="column"
+              >
+                <Box
+                  m={1}
+                  textAlign="right"
+                  position="absolute"
+                  top={170}
+                  left="60%"
+                  className={classes.display_macbook}
+                >
+                  <img src={MacBook} alt="macbook" width="450" />
+                </Box>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Container>
 
-      <Container
-        disableGutters={true}
-        maxWidth="xl"
-        style={{ backgroundColor: "grey.300", height: "100vh" }}
-      ></Container>
+        {/* Second Container */}
+        <Container
+          disableGutters={true}
+          maxWidth="xl"
+          style={{ backgroundColor: "grey.300", height: "65ch" }}
+        ></Container>
 
       {/* Third container */}
       <TrustBadgeContainer classes={classes} />
@@ -70,5 +136,6 @@ export default function Body() {
         </Typography>
       </Container>
     </React.Fragment>
+    </MuiThemeProvider>
   );
 }
