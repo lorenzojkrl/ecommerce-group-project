eefb6bfcf84acd346ed35922adb7fc143c7718bb
Eugene <gzkgt11@gmail.com>
2021-06-14 21:37:44 +0800

    Rename file & add add responsive fontsize

--- frontend/src/theme-style/materialtheme.js
+++ frontend/src/theme-style/materialtheme.js
@@ -1,27 +1,35 @@
-import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import {
+  createMuiTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+  MuiThemeProvider,
+} from "@material-ui/core";
 
-const Theme = ({children}) => {
+const Theme = ({ children }) => {
+  let theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#2F1160",
+      },
+    },
+    typography: {
+      fontFamily: ['"Roboto"'],
+      fontSize: 14,
+      h2: {
+        fontWeight: 750,
+      },
+    },
+  });
 
-    const theme = createMuiTheme({
-        palette: {
-            primary: {
-                main: "#2F1160"
-            }
-        },
-        typography: {
-            fontFamily: ['"Roboto"'],
-            fontSize: 14,
-            h2: {
-                fontWeight: 750
-            }
-        }
-    });
+  theme = responsiveFontSizes(theme);
 
-    return (
-        <ThemeProvider theme={theme}>
-            {children}
-        </ThemeProvider>
-    )
+  return (
+    <MuiThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        {children}
+      </ThemeProvider>
+    </MuiThemeProvider>
+  );
 };
 
-export { Theme }
+export { Theme };
