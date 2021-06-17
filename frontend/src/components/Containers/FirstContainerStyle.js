5980310dccf9b37feb6ce9b8a38aa374e7f9d5b2
Eugene <gzkgt11@gmail.com>
2021-06-17 12:19:27 +0800

    Fix responsive issue

--- frontend/src/components/Containers/FirstContainerStyle.js
+++ frontend/src/components/Containers/FirstContainerStyle.js
@@ -2,7 +2,6 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    fontWeight: 700,
     "& > *": {
       margin: theme.spacing(1),
       width: "25ch",
@@ -18,9 +17,14 @@ const useStyles = makeStyles((theme) => ({
   box_in_first_container: {
     position: "absolute",
     top: 160,
-    left: "13%",
+    left: "15%",
     lineHeight: 1,
     margin: 2,
+    [theme.breakpoints.between("xs", "sm")]: {
+      textAlign: "center",
+      top: 155,
+      right: "15vw",
+    },
   },
   subtitle_1: {
     color: "#2F1160",
@@ -32,11 +36,8 @@ const useStyles = makeStyles((theme) => ({
     left: "60%",
     textAlign: "right",
     margin: 1,
-    [theme.breakpoints.up("xs")]: {
+    [theme.breakpoints.between("xs", "sm")]: {
       display: "none",
-    },
-    [theme.breakpoints.up("sm")]: {
-      display: "flex",
     },
   },
 }));
