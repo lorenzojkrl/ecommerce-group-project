154fad5ecb7f723854780211877eb3977b740e91
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-29 19:45:12 +0800

    fix overflow on first container

--- client/src/components/Containers/FirstContainerStyles.js
+++ client/src/components/Containers/FirstContainerStyles.js
@@ -3,18 +3,15 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   first_container: {
     backgroundColor: "#FFF0E5",
-    // height: "75vh",
+    height: "75vh",
     textAlign: "center",
-    // [theme.breakpoints.down("sm")]: {
-    //   height: "100%",
-    //   paddingTop: "10vh",
-    //   paddingBottom: "10vh"
-    // },
+    [theme.breakpoints.down("xs")]: {
+      height: "90vh",
+    },
   },
   display_macbook: {
     width: "100%",
     [theme.breakpoints.down("xs")]: {
-      // display: "none",
       width: "calc(100% - 5em)"
     },
   },
