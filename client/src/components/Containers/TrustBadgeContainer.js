caafb82ee0a27b5e612e388e822c82004dd1cca0
Eugene <gzkgt11@gmail.com>
2021-06-29 10:02:14 +0800

    fix horizontal scroll

--- client/src/components/Containers/TrustBadgeContainer.js
+++ client/src/components/Containers/TrustBadgeContainer.js
@@ -1,104 +1,89 @@
-import {Grid, Typography , Card, CardMedia} from "@material-ui/core";
-import { useStyles } from './TrustBadgeContainerStyles'
+import { Grid, Typography, Card, CardMedia } from "@material-ui/core";
+import { useStyles } from "./TrustBadgeContainerStyles";
 
 const TrustBadgeContainer = () => {
-    const classes = useStyles(useStyles);
-    return (
-        <Grid
-            className={classes.root}
-            container
-            direction="row"
-            justify="space-evenly"
-            alignItems="center"
-            spacing={8}
-        >
-            <Grid
-                container
-                direction="row"
-                justify="space-evenly"
-                alignItems="center"
-                item xs={12}
-            >
-                <Typography
-                    variant="h2"
-                    align="center"
-                    color="primary"
-                >
-                Trusted By
-                </Typography>
-            </Grid>
-            <Grid
-                classes={{root: classes.cardGrid}}
-                container
-                direction="row"
-                justify="space-evenly"
-                alignItems="center"
-                item xs={12}
-                spacing={3}
-            >
-                <Grid item md={3} sm={6} xs={12}>
-                    <Card classes={{root: classes.card}}>
-                        <CardMedia
-                            className={classes.media}
-                            image="https://avatars.githubusercontent.com/u/73719327?v=4"
-                            title="Craig Norford"
-                            />
-                        <Typography
-                            variant="h6"
-                            align="center"
-                            >
-                        Craig Norford
-                        </Typography>
-                    </Card>
-                </Grid>
-                <Grid item md={3} sm={6} xs={12}>
-                    <Card classes={{root: classes.card}}>
-                        <CardMedia
-                            className={classes.media}
-                            title="Eugene Gohh"
-                            image="https://avatars.githubusercontent.com/u/64187129?v=4"
-                            />
-                        <Typography
-                            variant="h6"
-                            align="center"
-                        >
-                        Eugene Gohh
-                        </Typography>
-                    </Card>
-                </Grid>
-                <Grid item md={3} sm={6} xs={12}>
-                    <Card  classes={{root: classes.card}}>
-                        <CardMedia
-                            className={classes.media}
-                            title="Torey Littlefield"
-                            image="https://avatars.githubusercontent.com/u/52614742?v=4"
-                            />
-                       <Typography
-                            variant="h6"
-                            align="center"
-                            >
-                        Torey Littlefield
-                        </Typography>
-                    </Card>
-                </Grid>
-                <Grid item md={3} sm={6} xs={12}>
-                    <Card classes={{root: classes.card}}>
-                        <CardMedia
-                            className={classes.media}
-                            title="Lorenzo Zarantonello"
-                            image="https://avatars.githubusercontent.com/u/18583152?v=4"
-                            />
-                       <Typography
-                            variant="h6"
-                            align="center"
-                            >
-                        Lorenzo Zarantonello
-                        </Typography>
-                    </Card>
-                </Grid>
-            </Grid>
+  const classes = useStyles(useStyles);
+  return (
+    <Grid
+      className={classes.root}
+      container
+      direction="row"
+      justify="space-evenly"
+      alignItems="center"
+    >
+      <Grid
+        container
+        direction="row"
+        justify="space-evenly"
+        alignItems="center"
+        item
+        xs={12}
+      >
+        <Typography variant="h2" align="center" color="primary">
+          Trusted By
+        </Typography>
+      </Grid>
+      <Grid
+        classes={{ root: classes.cardGrid }}
+        container
+        direction="row"
+        justify="space-evenly"
+        alignItems="center"
+        item
+        xs={12}
+        spacing={3}
+      >
+        <Grid item md={3} sm={6} xs={12}>
+          <Card classes={{ root: classes.card }}>
+            <CardMedia
+              className={classes.media}
+              image="https://avatars.githubusercontent.com/u/73719327?v=4"
+              title="Craig Norford"
+            />
+            <Typography variant="h6" align="center">
+              Craig Norford
+            </Typography>
+          </Card>
         </Grid>
-    )
+        <Grid item md={3} sm={6} xs={12}>
+          <Card classes={{ root: classes.card }}>
+            <CardMedia
+              className={classes.media}
+              title="Eugene Gohh"
+              image="https://avatars.githubusercontent.com/u/64187129?v=4"
+            />
+            <Typography variant="h6" align="center">
+              Eugene Gohh
+            </Typography>
+          </Card>
+        </Grid>
+        <Grid item md={3} sm={6} xs={12}>
+          <Card classes={{ root: classes.card }}>
+            <CardMedia
+              className={classes.media}
+              title="Torey Littlefield"
+              image="https://avatars.githubusercontent.com/u/52614742?v=4"
+            />
+            <Typography variant="h6" align="center">
+              Torey Littlefield
+            </Typography>
+          </Card>
+        </Grid>
+        <Grid item md={3} sm={6} xs={12}>
+          <Card classes={{ root: classes.card }}>
+            <CardMedia
+              className={classes.media}
+              title="Lorenzo Zarantonello"
+              image="https://avatars.githubusercontent.com/u/18583152?v=4"
+            />
+            <Typography variant="h6" align="center">
+              Lorenzo Zarantonello
+            </Typography>
+          </Card>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
-export { TrustBadgeContainer }
+export { TrustBadgeContainer };
--- client/src/components/Containers/TrustBadgeContainerStyles.js
+++ client/src/components/Containers/TrustBadgeContainerStyles.js
@@ -1,31 +1,31 @@
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        backgroundColor: "#FFF0E5",
-        height: 500,
-        [theme.breakpoints.down('sm')]: {
-            height: "100%",
-            paddingBottom: "10vh"
-          },
-        textAlign: "center",
+  root: {
+    backgroundColor: "#FFF0E5",
+    height: "65vh",
+    [theme.breakpoints.down("sm")]: {
+      height: "100%",
+      paddingBottom: "10vh",
     },
-    card: {
-        boxShadow: "none",
-        backgroundColor: "inherit",
-        overflow: "hidden",
-        display: "inline-flex",
-        flexFlow: "column",
-        alignItems: "center",
-    },
-    cardGrid: {
-        // overflowX: "hidden"
-    },
-    media: {
-        height: 102,
-        width: 102,
-        borderRadius: "50%"
-    }
+    textAlign: "center",
+  },
+  card: {
+    boxShadow: "none",
+    backgroundColor: "inherit",
+    overflow: "hidden",
+    display: "inline-flex",
+    flexFlow: "column",
+    alignItems: "center",
+  },
+  cardGrid: {
+    // overflowX: "hidden"
+  },
+  media: {
+    height: 102,
+    width: 102,
+    borderRadius: "50%",
+  },
 }));
 
-export { useStyles }
+export { useStyles };
