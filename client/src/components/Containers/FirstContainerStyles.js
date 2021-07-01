06199263acf837aa1ab38463e0110b9db7a96b24
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-01 17:11:17 +0800

    adjust heights for responsiveness

--- client/src/components/Containers/FirstContainerStyles.js
+++ client/src/components/Containers/FirstContainerStyles.js
@@ -3,16 +3,16 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   first_container: {
     backgroundColor: '#FFF0E5',
-    height: 'min(75vh, 1080px)',
+    height: 'min(75vh, 500px)',
     textAlign: 'center',
-    [theme.breakpoints.between('xs', 'md')]: {
-      height: 'min(75vh, 500px)',
+    [theme.breakpoints.down('sm')]: {
+      height: 'min(85vh, 500px)',
     },
   },
   display_macbook: {
     width: '100%',
     [theme.breakpoints.down('xs')]: {
-      width: 'calc(100% - 5em)',
+      width: 'calc(100% - 6em)',
     },
   },
 }));
